242a095c187d3b142511b17b299027f214176a11
Ibrahim <ibra.b9438@gmail.com>
2021-09-06 00:46:22 +0300

    app.js

--- __test__/player.test.js
+++
@@ -1,8 +0,0 @@
-const { player } = require("../src/Player");
-const { gameBoard } = require("../src/Gameboard");
-
-beforeAll(() => {
-  const humanPlayer = player("Ibrahim");
-  const pcPlayer = player();
-  const board = gameBoard();
-});
--- src/app.js
+++
@@ -1,2 +0,0 @@
-import "../src/Gameboard";
-import "../src/shipFactory";
